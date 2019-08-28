f57d95bc150357f9031187943bb0ce0dfd29a5b9
Alex Pernot <alex_pernot@hotmail.com>
2019-08-28 10:24:59 +0200

    First commit, we can greyscale an image!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from 'react';
+import "./index.css";
+import Jimp from "jimp";
+import originalImageSrc from "./nox.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [imageSrc, setImageSrc] = useState(null);
+
+    useEffect(() => {
+        Jimp.read(originalImageSrc)
+            .then(image => {
+                image
+                    .resize(600, Jimp.AUTO)
+                    .greyscale()
+                    .getBase64(Jimp.MIME_JPEG, (err, src) => {
+                        setImageSrc(src);
+                    })
+            })
+            .catch(err => console.error(err))
+        ;
+    }, []);
+
+    return (
+        <div className="mx-auto mt-40">
+            <header>
+                <h1 className="text-center">Title card generator</h1>
+            </header>
+            <main className=" mx-auto">
+                {
+                    imageSrc ? <img src={imageSrc} alt="Custom"/> : <p>...</p>
+                }
+            </main>
+        </div>
+    );
 }
 
 export default App;
